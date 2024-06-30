e3086ed25bba7a4b8a3f47a5842dd11db45f7b40
nishii <jeewanthadeherath04@gmail.com>
2024-06-30 13:28:51 +0530

    transformed data to readable format

--- etl_script.sh
+++ etl_script.sh
@@ -5,3 +5,12 @@ echo "Extracting data"
 # 4 (visitorid)
 
 cut -d"#" -f1-4 webserver-access-log.txt>extracted_data.txt
+
+#Transform phase
+echo "Transforming data"
+
+#reading the extracted data and replace the colons with commas
+#write it to csv file
+
+tr "#" "," < extracted_data.txt>transformed_data.csv
+
